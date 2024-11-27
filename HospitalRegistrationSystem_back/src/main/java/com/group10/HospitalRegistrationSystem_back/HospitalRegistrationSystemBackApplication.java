3f5db0cc45ea620f44f025e7d7daeb19b29d55b0
Binfinity <binfinity@qq.com>
2024-11-27 08:16:40 +0800

    Create

---
+++ HospitalRegistrationSystem_back/src/main/java/com/group10/HospitalRegistrationSystem_back/HospitalRegistrationSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.group10.HospitalRegistrationSystem_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalRegistrationSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalRegistrationSystemBackApplication.class, args);
+	}
+
+}
---
+++ HospitalRegistrationSystem_back/src/test/java/com/group10/HospitalRegistrationSystem_back/HospitalRegistrationSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group10.HospitalRegistrationSystem_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRegistrationSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
